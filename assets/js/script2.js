8f086f193e2c164d12ef5472db16b176cf135341
MrMisterGabe <gabrieldelzingaro@gmail.com>
2021-08-02 18:10:41 -0400

    assign recipe title and recipe overview

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,18 +2,28 @@ var recipeDetailsEl = document.querySelector("#recipe-details")
 
 var detailedRecipe = function (data) {
 
+    var recipeListItemEl = document.createElement("div");
     var recipeImgEl = document.createElement("img");
-    recipeImgEl.setAttribute("src", data.baseUri + data.image);
+    recipeImgEl.setAttribute("src", data.image);
     recipeImgEl.setAttribute("id", "placeholder")
     var recipeInfoEl = document.createElement("div");
     recipeInfoEl.classList.add("recipe-info");
     var recipeTitleName = document.createElement("p");
+    var recipePrepTime = document.createElement("p");
+    var recipeCookTime = document.createElement("p");
+    var recipeTotalTime = document.createElement("p");
+    var recipeServings = document.createElement("p");
 
-    recipeTitleName.textContent = data.id.title;
-    recipeBakeTime.innerHTML = "Bake Time: " + data.id.readyInMinutes + " mins";
+
+
+    recipeTitleName.textContent = data.title;
+    recipePrepTime.textContent = data.preparationMinutes;
+    recipeCookTime.textContent = "Cook: " + data.cookingMinutes + " mins";
+    recipeTotalTime.textContent = "Total: " + data.readyInMinutes + " mins";
+    recipeServings.textContent = "Servings: " + data.servings;
 
     // Appending the elements to their appropriate sections.
-    recipeInfoEl.append(recipeTitleName, recipeBakeTime);
+    recipeInfoEl.append(recipePrepTime, recipeCookTime, recipeTotalTime, recipeServings);
     recipeListItemEl.append(recipeImgEl, recipeInfoEl);
     recipeDetailsEl.appendChild(recipeListItemEl);
 };
