a1d76359175162236ecfeeed15a18c2731f753a0
MrMisterGabe <gabrieldelzingaro@gmail.com>
2021-08-02 18:42:54 -0400

    set up variables for future use

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,29 +2,36 @@ var recipeDetailsEl = document.querySelector("#recipe-details")
 
 var detailedRecipe = function (data) {
 
+    // container for entire recipe article
     var recipeListItemEl = document.createElement("div");
+    // h2 for recipe title
+    var recipeTitleEl = document.createElement("h2");
+    // img
     var recipeImgEl = document.createElement("img");
-    recipeImgEl.setAttribute("src", data.image);
-    recipeImgEl.setAttribute("id", "placeholder")
-    var recipeInfoEl = document.createElement("div");
-    recipeInfoEl.classList.add("recipe-info");
+    // container for recipe overview
+    var recipeOverviewEl = document.createElement("div");
     var recipeTitleName = document.createElement("p");
     var recipePrepTime = document.createElement("p");
     var recipeCookTime = document.createElement("p");
     var recipeTotalTime = document.createElement("p");
     var recipeServings = document.createElement("p");
+    recipeImgEl.setAttribute("src", data.image);
+    recipeImgEl.setAttribute("id", "placeholder")
+    recipeOverviewEl.classList.add("cook-overview");
 
-
-
+    // recipe title
     recipeTitleName.textContent = data.title;
-    recipePrepTime.textContent = data.preparationMinutes;
+    
+    // recipe overview
+    recipePrepTime.textContent = "Prep: " + data.preparationMinutes;
     recipeCookTime.textContent = "Cook: " + data.cookingMinutes + " mins";
     recipeTotalTime.textContent = "Total: " + data.readyInMinutes + " mins";
     recipeServings.textContent = "Servings: " + data.servings;
 
-    // Appending the elements to their appropriate sections.
-    recipeInfoEl.append(recipePrepTime, recipeCookTime, recipeTotalTime, recipeServings);
-    recipeListItemEl.append(recipeImgEl, recipeInfoEl);
+    // Append the recipe overview elements to their appropriate sections.
+    recipeTitleEl.append(recipeTitleName)
+    recipeOverviewEl.append(recipePrepTime, recipeCookTime, recipeTotalTime, recipeServings);
+    recipeListItemEl.append(recipeImgEl, recipeOverviewEl);
     recipeDetailsEl.appendChild(recipeListItemEl);
 };
 
